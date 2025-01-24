752479af77390006fa3704d2c243ad319d5e197f
wandile <nanashongwe@gmail.com>
2025-01-24 12:51:02 +0200

    Initial commit

---
+++ src/test/java/com/limko/System/Design/SystemDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limko.System.Design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

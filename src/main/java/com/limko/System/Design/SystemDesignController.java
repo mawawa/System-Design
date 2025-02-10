3f4a6639206d42517857ae28fc197dce57ee7052
wandile <nanashongwe@gmail.com>
2025-02-10 10:18:03 +0200

    l1 to l4 ....

--- src/main/java/com/limko/System/Design/SystemDesignController.java
+++ src/main/java/com/limko/System/Design/SystemDesignController.java
@@ -6,8 +6,27 @@ import org.springframework.web.servlet.ModelAndView;
 
 public class SystemDesignController {
 
+    //lecture 1
     @RequestMapping("/")
     public ModelAndView home(){
         return new ModelAndView("index.html");
+    }
+
+    //lecture 2
+    @RequestMapping("/requirementsAndUseCases")
+    public ModelAndView requirementsAndUseCases(){
+        return new ModelAndView("Requirements and Use cases.html");
+    }
+
+    //lecture 3
+    @RequestMapping("/classModelsAndDiagrams")
+    public ModelAndView classModelsAndDiagrams(){
+        return new ModelAndView("Class Models and Diagrams.html");
+    }
+
+    //lecture 4
+    @RequestMapping("/classDiagrams")
+    public ModelAndView classDiagrams(){
+        return new ModelAndView("Class Diagrams.html");
     }
 }

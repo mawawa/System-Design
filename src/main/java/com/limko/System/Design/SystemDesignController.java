9b6534759dc53f0cf6cf8ec38dd3501b033853bd
wandile <nanashongwe@gmail.com>
2025-02-10 08:56:25 +0200

    lecture one more work coming

--- src/main/java/com/limko/System/Design/SystemDesignController.java
+++ src/main/java/com/limko/System/Design/SystemDesignController.java
@@ -1,5 +1,13 @@
 package com.limko.System.Design;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 public class SystemDesignController {
+
+    @RequestMapping("/")
+    public ModelAndView home(){
+        return new ModelAndView("index.html");
+    }
 }
